1246e284ae3fc7fb59de762a83a933b31ca2c1cf
8NOXX <iam8noxx@gmail.com>
2026-08-27 17:58:48 -0700

    Learn python variables

---
+++ 01-variables/practice.py
@@ -0,0 +1,5 @@
+name = input("Enter a name: ")
+age =int(input("Enter your age: "))
+
+print(f"My name is {name}.")
+print(f"I am {age} years old.")
---
+++ 01-variables/variables.py
@@ -0,0 +1,7 @@
+name = "Prashant Dangi"
+age = 17
+height = 5.6
+
+print(name)
+print(age)
+print(height)
