b911d3d240b4a7a3e2f69475cbc7287fc540b9d0
8NOXX <iam8noxx@gmail.com>
2026-08-28 14:11:51 -0700

    Learn an operators with examples.

---
+++ Operators/01-operators.py
@@ -0,0 +1,15 @@
+
+# simple calculation using operators:-
+
+a = 12
+b = 23
+
+c = a +b
+d = a -b
+e = a/b
+f = a *b
+
+print(f"Addition is {c}")
+print(f"substraction is {d}")
+print(f"Division is {e}")
+print(f"Multiply is {f}")
---
+++ Operators/02-operators.py
@@ -0,0 +1,16 @@
+# using input function and operators made a simple calculation:-
+
+num1 = float(input("Enter 01-number: "))
+num2 = float(input("Enter 02-Number: "))
+
+sum = num1 + num2
+multi = num1 * num2
+sub = num1 - num2
+div = num1 / num2
+
+print("<<<<<<<<<<<<< CALCULATION OF TWO NUMBER>>>>>>>>>>>>>>>")
+
+print(f"Sum of two number is {sum}")
+print(f"Multiplication of two number is {multi}")
+print(f'Substractio of two number is {sub}')
+print(f"Division of two number is {div}")
