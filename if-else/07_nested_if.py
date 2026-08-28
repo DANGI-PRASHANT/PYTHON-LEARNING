b482900fcb99776e0300106a0b0d4d22cd97b282
8NOXX <iam8noxx@gmail.com>
2026-08-28 16:03:56 -0700

    update code

---
+++ if-else/07_nested_if.py
@@ -0,0 +1,49 @@
+
+                                # ATM withdraw system: 
+
+# card = True
+
+# pin = int(input("Enter a pin: "))
+
+# if pin == 2082:
+   
+
+#     balance = int(input("enter a balance: "))
+
+#     if balance >=100:
+#         print("withdraw successful")
+#     else:
+#         print(" NO withdraw ")
+# else: 
+#     print("incorrect pin")
+    
+                            # Login system:
+
+# username = str(input("Enter your username: "))
+
+# if username.lower() == "ram@123":
+    
+
+#     password = str(input("Enter password: "))
+#     if password == "Hari@#123":
+#         print("Login successful")
+#     else: 
+#         print("incorrect password")
+# else: 
+#     print("invalid username")
+
+
+                            # Sinmple invition on house door system:
+friend = True
+invitation = True
+
+if friend:
+
+    if invitation:
+            print("come inside")
+    else:
+          print("please wait outside")
+else: 
+      print("sorry")
+
+
