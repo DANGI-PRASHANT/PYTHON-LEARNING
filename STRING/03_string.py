9aa3fe7f138c6a05c8004b398e9212cc616aa920
8NOXX <iam8noxx@gmail.com>
2026-09-06 22:54:08 -0700

    Add some file

---
+++ STRING/03_string.py
@@ -0,0 +1,38 @@
+#1. Create a string name = Ram and print it in uppercase and lowercase.
+
+name = "Ram"
+
+print(name.lower())
+print(name.upper())
+
+# 2.Store a sentence Python is very easy and print its length.
+
+word = "Python"
+
+print(len(word))
+
+# 3. Create a string word = programming and print the first and last character.
+
+word_01 = "programming"
+
+print(word_01[0])
+print(word_01[-1])
+
+# 4.Store a string message = Hello World and print only World using slicing.
+
+message = "Hello world"
+
+print(message[:5])
+print(message[::-1])
+print(message[3:7])
+
+# 5.Create a string text = python and print it in reverse order.
+
+text = "python"
+
+print(text[::-1])
+
+# 6. Store a string name = Ram and repeat it 3 times using operator.
+name_01 = "Ram"
+
+print("Ram\n" *3)
