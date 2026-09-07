8949f2ac0a8ab36f6957ff2a7a6d703809107e0b
8NOXX <iam8noxx@gmail.com>
2026-09-06 17:24:55 -0700

    learn something new

--- SETS/03_sets.py
+++ SETS/03_sets.py
@@ -2,10 +2,23 @@
 
 # A. Remove:
 
-fruits = {"apple","orange","mango"}
+# fruits = {"apple","orange","mango"}
 
-fruits.remove("apple")
-print(fruits)
+# fruits.remove("apple")
+# print(fruits)
 
 # B. Discard: 
 
+# name = {"ram","shyam","krishna","hari"}
+
+# name.discard("ram")
+# print(name) # no error when item is not in list.
+
+
+# C. pop():
+
+fruits_01 = {"apple","orange","mango"}
+
+removed = fruits_01.pop()
+print(fruits_01)
+print(f"{removed} deleted")
